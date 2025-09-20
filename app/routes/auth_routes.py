f8b7a1955cba387a0689fa4dd519dd492a05b74f
gitesh.mane21@vit.edu <giteshmane013>
2025-09-20 13:54:14 +0530

    added routes.py

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,23 +1,19 @@
+from fastapi import APIRouter, status
 
-from fastapi import APIRouter
+from app.schemas.auth_schemas import RegisterRequest,LoginRequest,UserResponse,TokenResponse
+from app.services.auth_service import AuthService
 
-router = APIRouter()
+router = APIRouter(prefix="/api/auth", tags=["auth"])
+auth_service = AuthService()
 
+@router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+def register_user(payload: RegisterRequest):
+    return auth_service.register_user(payload.username, payload.email, payload.password)
 
-@router.get("/register")
-def resgister_user():
-    return {"status": "ok"}
-
-
-@router.post("/register")
-def create_user():
-    return {"status": "ok"}
-
+@router.post("/login", response_model=TokenResponse, status_code=status.HTTP_200_OK)
+def login_user(payload: LoginRequest):
+    return auth_service.login_user(payload.username, payload.password)
 
 @router.get("/login")
-def health_check(): 
-    return {"status": "ok"}
-
-@router.post("/login")
-def login_user():
+def login_health_check():
     return {"status": "ok"}
